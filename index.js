b320cdd22603828d612d8d227e95e823dda5e946
Shalini1908 <singhparmar1908@gmail.com>
2023-02-19 13:16:32 +0530

    backendDeploy2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {noteRouter} = require("./routes/Note.routes");
 const {authenticate} = require("./middleware/authenticate.middleware");
 const cors = require("cors")
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin:"*"}))
 
 app.get("/",(req,res)=>{
 res.send("Welcome to my Website")
